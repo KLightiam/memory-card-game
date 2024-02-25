fe3ce66e80838f326c3cf959b50b94c8e38047f8
kingsley <kingsleyasareankomah@gmail.com>
2024-02-25 20:02:51 +0000

    fix randomisation of cards

--- src/components/shuffleArr.js
+++ src/components/shuffleArr.js
@@ -1,9 +1,15 @@
 
 
 export default function shuffleArr(arr){
-    for(let i = arr.length -1;i>0;i--){
+    for(let i = arr.length - 1;i > 0;i--){
         const j = Math.floor(Math.random()*(i+1));
         [arr[i],arr[j]] = [arr[j],arr[i]];
-        return arr;
     }
+    return arr;
 }
+
+// function checkRandomVal(a,b){
+//     const d = Math.floor(Math.random()*(i+1))
+//     const c = (a == b) ? checkRandomVal(a,d) : b;
+//     return c;
+// }
